ae2da918356bec746fb6f37ea24e703b5e0c6ed2
mahmudurmahid <m.mahid@icloud.com>
2025-07-16 13:37:02 +0100

    triple like button works

--- how-react-works-app/src/App.js
+++ how-react-works-app/src/App.js
@@ -66,7 +66,16 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleTripleInc() {
+    // setLikes((likes) => likes + 1);
+    // setLikes((likes) => likes + 1);
+    // setLikes((likes) => likes + 1);
+    handleInc();
+    handleInc();
+    handleInc();
   }
 
   function handleUndo() {
@@ -87,13 +96,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
         <button onClick={handleUndo}>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
